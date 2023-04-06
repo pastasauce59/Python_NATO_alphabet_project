ea10f3ee367d755c450fa6c3688076be43261c2f
Michael Primavera <pastasauce59@gmail.com>
2023-04-05 21:51:06 -0400

    Created phonetic dict from csv using dict comprehension, then created a phonetic list using the user's input with list comprehension.

--- main.py
+++ main.py
@@ -20,8 +20,16 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
+- - - - - -
+
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
+nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Enter a word: ").upper()
+phonetic_list = [nato_dict[char] for char in user_input]
+print(phonetic_list)
